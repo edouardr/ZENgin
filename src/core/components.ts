b2b2805dfcc9687c09f27131ed605b8b3cbb4cd9
EdouardR <riviere.edouard@hotmail.fr>
2016-09-25 17:20:32 +1000

    Messaging class

--- src/core/components.ts
+++ src/core/components.ts
@@ -1,3 +1,127 @@
 
+import { Messaging } from "../_utils"
+
+/**
+ * 
+ * 
+ * @export
+ * @enum {number}
+ */
+export const enum COMPONENTS_TYPE {
+    KEYBOARD,
+    COLLIDER
+}
+
+/**
+ * 
+ * 
+ * @export
+ * @interface IComponent
+ */
 export interface IComponent {
+
+    /**
+     * 
+     * 
+     * @param {Common.IMessage} msg
+     */
+    sendMessage(msg: Messaging.IMessage): void
+
+    /**
+     * 
+     * 
+     * @param {number} dt
+     */
+    update(dt: number ): void
+    
+    /**
+     * 
+     */
+    init(): void
+    
+    /**
+     * 
+     */
+    shutDown(): void
+    
+    /**
+     * 
+     * 
+     * @returns {boolean}
+     */
+    isActive(): boolean
+}
+
+
+/**
+ * 
+ * 
+ * @export
+ * @abstract
+ * @class Component
+ * @implements {IComponent}
+ */
+export abstract class Component implements IComponent {
+
+    /**
+     * 
+     * 
+     * @private
+     * @type {boolean}
+     */
+    protected _active: boolean
+    
+    /**
+     * ComponentId
+     * 
+     * @private
+     * @type {*}
+     */
+    protected _componentId: COMPONENTS_TYPE 
+    
+    /**
+     * GameObjectHandle
+     * 
+     * @private
+     * @type {*}
+     */
+    protected _handle: any 
+
+    /**
+     * 
+     * 
+     * @abstract
+     * @param {Common.IMessage} msg
+     */
+    abstract sendMessage(msg: Messaging.IMessage): void
+
+    /**
+     * 
+     * 
+     * @abstract
+     * @param {number} dt
+     */
+    abstract update(dt: number ): void
+    
+    /**
+     * 
+     * 
+     * @abstract
+     */
+    abstract init(): void
+    
+    /**
+     * 
+     * 
+     * @abstract
+     */
+    abstract shutDown(): void
+    
+    /**
+     * 
+     * 
+     * @abstract
+     * @returns {boolean}
+     */
+    abstract isActive(): boolean
 }
