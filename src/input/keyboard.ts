95a1e3a5e43798ddfebdb204b1ec04666d9e320b
EdouardR <riviere.edouard@hotmail.fr>
2016-09-25 17:35:56 +1000

    Input controller + keyboard

--- src/input/keyboard.ts
+++ src/input/keyboard.ts
@@ -1,27 +1,111 @@
 import * as Components from "../_components"
-import { Common } from "../_utils"
+import { Messaging } from "../_utils"
+import { ICreator } from "../objectFactory/creator"
 
-    export interface IKeyBoard extends Components.IComponent {
-        keyState: Array<boolean>,
-        isDown: (keyCode: number) => boolean
+export interface IKeyBoard {
+    keyState: Array<boolean>,
+    isDown: (keyCode: number) => boolean
+}
+
+export class KeyBoard extends Components.Component implements IKeyBoard {
+
+    keyState: Array<boolean>
+
+    /**
+     * 
+     * 
+     * @type {HTMLCanvasElement}
+     */
+    canvas: HTMLCanvasElement
+
+    /**
+     * Creates an instance of KeyBoard.
+     * 
+     * @param {HTMLCanvasElement} canvas
+     */
+    constructor(canvas: HTMLCanvasElement) {
+        super();
+        this.canvas = canvas;
+        this.init();
     }
 
-    export class KeyBoard implements IKeyBoard {
-        keyState: Array<boolean>
-        constructor(canvas: HTMLCanvasElement) {
-            var self = this;
-
-            canvas.addEventListener("keydown", function (event) {
-                self.keyState[event.keyCode] = true;
-            });
-
-            canvas.addEventListener("keyup", function (event) {
-                self.keyState[event.keyCode] = false;
-            });
-        }
-
-        isDown(keyCode: number) {
-            return this.keyState[keyCode] === true;
-        }
+    /**
+     * 
+     * 
+     * @param {number} dt
+     * @returns
+     */
+    update(dt: number) {
+        return;
     }
 
+    /**
+     * 
+     * 
+     * @returns
+     */
+    init() {
+        this._active = true;
+        var self = this;
+
+        this.canvas.addEventListener("keydown", function (event) {
+            self.keyState[event.keyCode] = true;
+        });
+
+        this.canvas.addEventListener("keyup", function (event) {
+            self.keyState[event.keyCode] = false;
+        });
+        return;
+    }
+
+    /**
+     * 
+     * 
+     * @param {Messaging.IMessage} msg
+     * @returns
+     */
+    sendMessage(msg: Messaging.IMessage) {
+        return;
+    }
+
+    /**
+     * 
+     * 
+     * @returns
+     */
+    isActive() {
+        return this._active;
+    }
+
+    /**
+     * 
+     * 
+     * @returns
+     */
+    shutDown() {
+        return;
+    }
+
+    /**
+     * 
+     * 
+     * @param {number} keyCode
+     * @returns
+     */
+    isDown(keyCode: number) {
+        return this.keyState[keyCode] === true;
+    }
+}
+
+/**
+ * 
+ * 
+ * @export
+ * @class GameObjectCreator
+ * @implements {ICreator}
+ */
+export class KeyBoardCreator implements ICreator {
+    create(properties: PropertyDescriptorMap): KeyBoard {
+        return new KeyBoard(properties["canvas"] as HTMLCanvasElement);
+    }
+}
